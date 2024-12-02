81902c7a86f2a32670debe15ca6953bb979574d1
tysoab <tysoab@gmail.com>
2024-12-02 09:47:37 +0100

    change code logic - used switch statement instead of if statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // select elements
 const converterForm = document.querySelector(".converter-form");
 const btnSelect = document.querySelector(".sel-opt");
-const degValue = document.querySelector(".value-el");
+const inputEl = document.querySelector(".value-el");
 const result = document.querySelector("#result");
 
 // helper
-function shortDec(num) {
-  const str = String(num).split(".");
+function shortDec(param) {
+  const str = String(param.temp).split(".");
   if (str[1] && str[1].length >= 3) {
-    return num.toFixed(2);
+    return `${param.temp.toFixed(2)}${param.unit}`;
   } else {
-    return num;
+    return `${param.temp}${param.unit}`;
   }
 }
 
@@ -21,29 +21,63 @@ converterForm.addEventListener("submit", (event) => {
   const opt = btnSelect.value;
 
   //   get the degree
-  let deg = degValue.value;
+  const temp = +inputEl.value;
 
-  //   convert string to number
-  deg = Number(deg);
-
-  if (opt === "") {
+  if (opt === "" || inputEl.value === "") {
     return;
   }
 
-  //   convert celsius to fahrenheit
-  if (opt === "c") {
-    // (0°C × 9/5) + 32
+  let calc;
 
-    const calc = (deg * 9) / 5 + 32;
+  switch (true) {
+    //   convert celsius to fahrenheit
+    case opt === "cf":
+      calc = {
+        temp: (temp * 9) / 5 + 32,
+        unit: "<sup>o</sup>F",
+      };
+      break;
 
-    result.innerHTML = shortDec(calc) + "<sup>o</sup>F";
+    // convert celsius to kelvin
+    case opt === "ck":
+      calc = {
+        temp: temp + 273.15,
+        unit: "K",
+      };
+      break;
+
+    // convert fahrenheit to celsius
+    case opt === "fc":
+      calc = {
+        temp: (temp - 32) * (5 / 9),
+        unit: "<sup>o</sup>C",
+      };
+      break;
+
+    // convert fahrenheit to kelvin
+    case opt === "fk":
+      calc = {
+        temp: (temp - 32) * (5 / 9) + 273.15,
+        unit: "K",
+      };
+      break;
+
+    // convert kelvin to celsius
+    case opt === "kc":
+      calc = {
+        temp: temp - 273.15,
+        unit: "<sup>o</sup>C",
+      };
+      break;
+
+    // convert kelvin to fahrenheit
+    case opt === "kf":
+      calc = {
+        temp: (temp - 273.15) * (9 / 5) + 32,
+        unit: "<sup>o</sup>Fs",
+      };
+      break;
   }
 
-  //   convert fahrenheit to celsius
-  if (opt === "f") {
-    // (32°F − 32) × 5/9
-    const calc = ((deg - 32) * 5) / 9;
-
-    result.innerHTML = shortDec(calc) + "<sup>o</sup>C";
-  }
+  result.innerHTML = shortDec(calc);
 });
